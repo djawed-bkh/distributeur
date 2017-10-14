88172c95df300d742396388a40cdc88cc60ff90a
djawed-bkh <bekhouchadjawed@gmail.com>
2017-10-14 23:46:45 +0100

    14oct17

--- src/compte.java
+++
@@ -1,9 +0,0 @@
-public class compte {
-    private int dollar;
-    private int euro;
-
-    public compte(int dollar, int euro) {
-        this.dollar = dollar;
-        this.euro = euro;
-    }
-}
